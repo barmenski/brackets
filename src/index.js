160ce487bd89d75f46c06d794c6f7adff03a2698
barmenski <1kiloom@gmail.com>
2021-02-03 01:44:36 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,69 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let openBr=[];
+  let closeBr=[];
+  let openIndex;
+  let closeIndex;
+  let strArr=[];
+  let strSame=[];
+
+  bracketsConfig.forEach(element => {
+    openBr.push(element[0]);
+    closeBr.push(element[1]);    
+  });
+
+  for(let i=0; i<=openBr.length-1; i++){
+    if(openBr[i]===closeBr[i]){
+      strSame.push(openBr[i]);
+    }
+  }
+  
+  if (strSame.length>0){
+    for(let i=0; i<=closeBr.length-1; i++){
+      for(let j=0; j<=strSame.length-1; j++){
+        if(closeBr[i]===strSame[j]){
+          closeBr[i]=String.fromCodePoint(65+i);
+        }
+      }
+    }
+    
+    for(let i=0; i<str.length; i++){
+      if(strSame.includes(str[i])){
+        openIndex=openBr.indexOf(str[i]);
+        if(openIndex===stack[stack.length-1]){
+          strArr.push(closeBr[openIndex]);
+          stack.pop();
+        } else {
+          strArr.push(str[i]);
+          stack.push(openIndex);
+        }                   
+      } else {
+        strArr.push(str[i]);
+      }
+    }
+    str=strArr.join("");
+
+  }
+
+  for (let i = 0; i <str.length; i++) {
+     openIndex = openBr.indexOf(str[i]);
+     if (openIndex !== -1) {
+         stack.push(openIndex);
+         continue;
+     }
+
+     closeIndex = closeBr.indexOf(str[i]);
+     if (closeIndex !== -1) {
+         openIndex = stack.pop();
+         if (closeIndex !== openIndex) {
+             return false;
+         }
+     }
+  }
+
+  if (stack.length !== 0) {
+      return false;
+  }
+
+  return true;
 }
